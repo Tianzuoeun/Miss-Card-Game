e58fab7f9980812f1b6d3dcf0bfbe311856f3059
Yiqing Sha <ysha@smu.edu>
2023-09-07 10:26:22 -0500

    Initial Commit

---
+++ Miss Card Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Miss Card Game
+//
+//  Created by howardshan on 9/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
